c794cff027cb71a926a6497a7e0893f915c1cca3
Farrel Mohammad <farrel.adel@gmail.com>
2022-06-29 17:49:32 +0700

    update frontend

--- frontend/script2.js
+++ frontend/script2.js
@@ -1,8 +1,15 @@
 window.addEventListener('scroll', scrollHeader)
 
 const login_btn = document.getElementById('login');
+const exit_btn = document.getElementById('exit');
 
 login_btn.addEventListener('click', function(){
     const login_form = document.querySelector('.center');
     login_form.classList.toggle('show');
 })
+
+exit_btn.addEventListener('click', function(){
+    const login_form = document.querySelector('.center');
+    login_form.classList.remove('show');
+})
+
